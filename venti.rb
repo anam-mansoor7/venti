4bea0750ff69465f6dd6f9aebc08de4b09cfa1f5
anam <anam.mansoor@7vals.com>
2014-03-27 11:06:24 +0500

    base 32 encoding

--- venti.rb
+++ venti.rb
@@ -1,54 +1,73 @@
 require 'digest/sha1'
 require 'pry'
+require "base32"
 
 class Venti
-	def initialize
-		@block_size = 100
-		@directory = 'venti'
+  def initialize
+    @block_size = 100
+    @directory = 'venti'
     Dir.mkdir @directory unless File.directory?(@directory)
   end
   
   #TODO: remove default value of file name
-  def vac(filename = nil)
-  	data             = File.read('test.txt')
-  	number_of_blocks = (data.size.to_f / 100).ceil
-  	addresses        = []
-  	
-  	for block in 0...number_of_blocks 
-  		istart = block * 100
-      iend   = istart + 99  
-      block_data = data[istart..iend]
-  		addresses[block] = Digest::SHA1.digest block_data
-  		write_block(addresses[block], block_data)
-  	end 
+  def vac(filename = 'test.txt')
+    data = File.read(filename)
+    address_list = write_blocks(data, 100)
 
-  	length     = (addresses.size.to_f / 5).ceil
+    #TODO: further investigate the condition whether to set address_list to nil on the first step?
+    while address_list.size != 1 do
+      address_list = write_blocks(address_list, 5)	
+    end
 
+    vacfile = filename.chomp(File.extname(filename)) + ".vac"
 
+    #convert the hash to bin before writing
+    file_details = {name: filename, size: data.size, address: address_list.first}
+
+    File.open(vacfile , "wb") do |file| 
+      file.write(Marshal.dump(file_details))
+    end 
+  end
+
+  def unvac(filename)
+    #file_details = File.read(filename).gsub(/[{}:]/,'').split(', ').map{|h| h1,h2 = h.split('=>'); {h1 => h2}}.reduce(:merge)
+    file_details = Marshal.load(File.read(filename))
+    binding.pry
+    file = @directory + '/' + Base32.encode(file_details[:address]) + '.dat'
+    contents = File.read(file)
+     binding.pry
   end
 
   private
+  
+  #range = 5 for pointer blks and 100 for data blks
+  def write_blocks(data, range)
+    address_list = []
+    length       = (data.size.to_f / range).ceil
 
-  def write_block(address, data)
-  	encoded_address  = @directory + '/'+ addresses.encode('utf-8', 'iso-8859-15') + '.dat'
-    #TODO: make the .dat file hidden and binary
-		unless File.file?(filename)
-  		File.open(encoded_address , "w+") do |file| 
-  			file.write(block_data)
-  		end
-  	end
+    for block in 0...length
+      istart = block * range
+      iend = istart + range
+      block_data = data[istart...iend]
+      block_data = block_data.join if range == 5
+      address_list[block] = Digest::SHA1.digest block_data 
+      write_block(address_list[block], block_data)   
+    end
+    address_list
   end
 
-  def write_blocks(data, length, range)
-    for i in 0...length
-  		istart      = i * range
-      iend        = istart + range
-      block_data  = data[istart...iend]
-      address[block] = Digest::SHA1.digest block_data
-      write_block(address[block], block_data)   
-  	end
+  def write_block(address, data)
+
+    block_address = @directory + '/'+ Base32.encode(address) + '.dat'
+    #TODO: make the .dat file hidden and binary
+    unless File.file?(block_address)
+      File.open(block_address , "wb") do |file| 
+        file.write(data)
+      end
+    end
   end
 end
 
 v = Venti.new
-v.vac
+v.vac('test.txt')
+v.unvac('test.vac')
